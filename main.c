3bc19d8af032f6e9ce24a2df3387e1c7910e35af
Petr Gaube <petr.gaube@lodhart.com>
2024-05-22 10:38:03 +0200

    New files

---
+++ main.c
@@ -0,0 +1,13 @@
+// New version v2_branch
+// Test, change on GitHub
+void main(void) {
+
+  printf("v4aa_fssbranch - next");
+
+  // Continuing witdfdfh master ... master agaieden
+
+
+  return 0;
+}
+// 5446465465
+// My new change
